2cf0e79322983ad0650a2f9a621a72d84763bfcd
YJS <y.j3ms.n@gmail.com>
2024-02-24 20:35:49 +0000

    refactor: remove unnecessary label and directive
    - remove .global directive
    - remove _start label

--- example/hello.s
+++ example/hello.s
@@ -1,13 +1,11 @@
-.global _start
 .text
 
-_start:
-    mov x0, #1                         /* file descriptor of stdout */
-    adr x1, msg                        /* get current address and add offset to `msg` label */
-    ldr x2, =msg_len                   /* length of the `msg` string */
-    mov x8, #64                        /* write system call number for aarch64 */
-    svc #0                             /* invoke system call */
-    ret
+mov x0, #1                             /* file descriptor of stdout */
+adr x1, msg                            /* get current address and add offset to `msg` label */
+ldr x2, =msg_len                       /* length of the `msg` string */
+mov x8, #64                            /* write system call number for aarch64 */
+svc #0                                 /* invoke system call */
+ret
 
 msg:
     .ascii "Hello from assembly!\n"    /* string is placed directly in the text section */
